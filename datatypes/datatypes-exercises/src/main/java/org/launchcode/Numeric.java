986c0b5fbceaf0d67de1dcad4ac637ee6a5ee53b
Zachary Cain <zacharylcain@gmail.com>
2024-04-01 21:18:04 -0500

    successfully solved for the Area of the rectangle.

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/Numeric.java
@@ -0,0 +1,21 @@
+package org.launchcode;
+
+import java.util.Scanner;
+public class Numeric {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double recArea;
+
+        System.out.println("What is the Base of the Rectangle?");
+        Double recBase = input.nextDouble();
+
+        System.out.println("What is the Height of the Rectangle?");
+        Double recHeight = input.nextDouble();
+
+        System.out.println("What is the Width of the Rectangle.");
+        Double recWidth = input.nextDouble();
+
+         recArea = recBase * recHeight * recWidth;
+        System.out.println("The Area of the Rectangle is: " + recArea + "Ft.");
+    }
+}
